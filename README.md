d3029ffa94aaf4eab2d7dda5393ba7601369222d
Konstantin Kotschenreuther <kkotsche1@gmail.com>
2023-04-10 15:46:33 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Python code to convert LLaMA Weights to Huggingface format locally. 
+
+
+Please make sure to adjust the Model Size, Input Directory, and Output directory in the main.py file. 
+
---
+++ main.py
@@ -0,0 +1,22 @@
+import subprocess
+
+#Installing requirements
+command = ["pip", "install", "-r", "requirements.txt"]
+subprocess.run(command)
+
+command = ['pip', 'uninstall', 'transformers']
+subprocess.run(command)
+
+#Installing transformers library from github
+
+command = ['pip', 'install', 'git+https://github.com/huggingface/transformers']
+subprocess.run(command)
+
+
+model_size = "7B"
+input_dir = 'C:/Users/Admin/Downloads/LLaMA'
+output_dir = './vicuna_7b_hf'
+
+#Conversion
+command = ['python', 'convert_llama_weights_to_hf.py', '--input_dir', input_dir, '--model_size', model_size, '--output_dir', output_dir]
+subprocess.run(command)
